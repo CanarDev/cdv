0c8f55650f7b2ff05202a4b38ce055bd78e38203
CanarDev <vincentmichelnavarro@gmail.com>
2024-11-19 15:15:26 +0100

    :bug: Update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import SceneBouncingBubbles from "./js/scenarios/SceneBouncingBubbles"
+import {askMotionAccess} from "./js/Utils/askMotionAccess";
 
 const scene2d = new SceneBouncingBubbles("canvas-scene")
 const scene2d2 = new SceneBouncingBubbles("canvas-scene2")
+
+
+const btn = document.getElementById('btn-access')
+btn.addEventListener('click', function() {
+    askMotionAccess()
+},false)
