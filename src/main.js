d1f9c0a6d99d913fcc5620dee7575dce3c988147
CanarDev <vincentmichelnavarro@gmail.com>
2024-11-21 12:36:55 +0100

    :sparkles: Update repo from course

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
-import SceneBouncingBubbles from "./js/scenarios/SceneBouncingBubbles"
-import {askMotionAccess} from "./js/Utils/askMotionAccess";
-import SceneGravityCubes from "./js/scenarios/SceneGravityCubes";
+import Scene2D from "./js/Scene2D"
 
-const scene2d = new SceneBouncingBubbles("canvas-scene")
-const scene3d = new SceneGravityCubes("canvas-scene3d")
-const scene2d2 = new SceneBouncingBubbles("canvas-scene2")
-
-
-const btn = document.getElementById('btn-access')
-btn.addEventListener('click', function() {
-    askMotionAccess()
-},false)
+const scene2d = new Scene2D()
